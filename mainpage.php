af8becc4c746eefa07cfa471fae54c6cc23a4f45
Ashstarfire <fahmeenmazhar@gmail.com>
2019-06-18 14:09:14 +0500

    Add files via upload

---
+++ mainpage.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>E-khidmat</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php require('css/styles.php'); ?>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+	
+	<!--start header  area --> 
+	<?php require 'header.php'; ?>
+    <!--end header  area --> 
+    <!--Start nav  area --> 
+	<?php require 'nav_area.php'; ?>
+	<!--end nav  area -->
+    <!--Start slider  area --> 
+	<?php require 'slider_area.php'; ?>
+	<!--end slider  area -->
+    <!--Start about  area --> 
+	<div class="about_area">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<div class="content-inner">
+						<h5 class="module-title">
+							Welcome to <span>E-khidmat</span>
+						</h5>
+						<div class="content-desc">
+							<div class="">
+								<p class="lead"></p>
+							</div>
+						  </div>
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<div class="video-wrapper pull-right">
+					<img style="height:400px; width:560px;"src="images/degreecap.jpg" alt="" />
+						<!--<a class="venobox_custom mfp-iframe vidwrap" data-type="iframe" href="https://www.youtube.com/watch?v=0JOsRybtsEk"></a>-->
+					 </div>				
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--end about  area -->	
+	<!--start share  area -->
+	<div class="share_area sharre_top">
+		<div class="container">
+			<div class="row">
+				<div class="share-container">
+					<!-- single item brand -->
+					<div class="col-md-3 col-sm-6 col-lg-3">
+						<div class="share-item">
+							<div class="brand_content content_left_fb">
+								
+									<i class="fa fa-facebook"></i>
+									<div class="icone_text">
+									<p>711K Followers</p>
+									<h5>Follow Us</h5>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+					<!-- end single item brand -->
+					<!-- single item brand -->
+					<div class="col-md-3 col-sm-6 col-lg-3">
+						<div class="share-item">
+							<div class="brand_content content_left_tw">
+								
+									<i class="fa fa-twitter"></i>
+									<div class="icone_text">
+									<p>890K Followers</p>
+									<h5>Follow Us</h5>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+					<!-- end single item brand -->
+					<!-- single item brand -->
+					<div class="col-md-3 col-sm-6 col-lg-3">
+						<div class="share-item">
+							<div class="brand_content content_left_pn">
+								<a href="https://fui.furc.edu.pk/">
+									<i class="fa fa-google"></i>
+									<div class="icone_text">
+									<p>120K Followers</p>
+									<h5>Follow Us</h5>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+					<!-- end single item brand -->
+					<!-- single item brand -->
+					<div class="col-md-3 col-sm-6 col-lg-3">
+						<div class="share-item">
+							<div class="brand_content content_left_in">
+								
+									<i class="fa fa-linkedin"></i>
+									<div class="icone_text">
+									<p>500K Followers</p>
+									<h5>Follow Us</h5>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+					<!-- end single item brand -->				
+				</div>
+			</div>
+		</div>
+	</div>	
+	<!--end share  area -->	
+
+	<!-- footer bottom area -->
+	<div class="footer_bottom_area">
+		<div class="container">
+			<div class="row">
+				<div class=" col-sm-6 col-md-6 col-lg-6">
+					<div class="footer_text">
+						
+						<p>
+							LMS © 2019 All Rights Reserved. 
+						</p>
+					</div>
+				</div>
+				<div class=" col-sm-6 col-md-6 col-lg-6">
+					
+				</div>				
+			</div>
+		</div>	
+	</div>		
+		<!-- jquery
+		============================================ -->		
+        <script src="js/vendor/jquery-1.11.3.min.js"></script>
+		<!-- bootstrap JS
+		============================================ -->		
+        <script src="js/bootstrap.min.js"></script>
+		<!-- wow JS
+		============================================ -->		
+        <script src="js/wow.min.js"></script>
+		 <!-- Nivo Slider JS -->
+		<script src="js/jquery.nivo.slider.pack.js"></script> 		
+		<!-- meanmenu JS
+		============================================ -->		
+        <script src="js/jquery.meanmenu.min.js"></script>
+		<!-- owl.carousel JS
+		============================================ -->		
+        <script src="js/owl.carousel.min.js"></script>
+		<!-- scrollUp JS
+		============================================ -->		
+        <script src="js/jquery.scrollUp.min.js"></script>
+		<!-- Apple TV Effect -->
+        <script src="js/atvImg-min.js"></script>
+		<!-- Add venobox js -->
+		<script type="text/javascript" src="venobox/venobox.min.js"></script>
+		<!-- plugins JS
+		============================================ -->		
+        <script src="js/plugins.js"></script>
+		<!-- main JS
+		============================================ -->		
+        <script src="js/main.js"></script>
+    </body>
+</html>
